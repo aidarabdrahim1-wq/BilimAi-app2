6394da6bb0b5b8680a37577acdd2fd2515985bf6
41ParadoxAidar abdrahim79Весторес <aidarabdrahim1@gmail.com>
2026-03-08 12:06:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5303827652-38d67",
+  "appId": "1:434230863781:web:17ed6d12afbccb348f2a4c",
+  "apiKey": "AIzaSyD4h_rErcBSKWhiI1U3CBpF8vDpWAfMRMY",
+  "authDomain": "studio-5303827652-38d67.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "434230863781"
+};

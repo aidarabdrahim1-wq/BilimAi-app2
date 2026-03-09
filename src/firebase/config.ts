6533b170e20b57e98f1c10536de820e4fdf6bcd6
41ParadoxAidar abdrahim79Весторес <aidarabdrahim1@gmail.com>
2026-03-09 03:10:13 +0000

    AIzaSyBKjwVhLCwkwmbC6IWiV9upggfVfm96lBM ауыстыр

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDxeU1ZG-SXr_H4v-96JqWioj6SX-x1KrM",
+  apiKey: "AIzaSyBKjwVhLCwkwmbC6IWiV9upggfVfm96lBM",
   authDomain: "studio-5303827652-38d67.firebaseapp.com",
   projectId: "studio-5303827652-38d67",
   storageBucket: "studio-5303827652-38d67.firebasestorage.app",

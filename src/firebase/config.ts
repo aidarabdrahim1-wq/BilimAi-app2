1f976818aa922447417e9184a29d835e36e4fd18
Your Name <aidarabdrahim1@gmail.com>
2026-03-09 05:14:40 +0000

    AIzaSyDxeU1ZG-SXr_H4v-96JqWioj6SX-x1KrM осыган озгер

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,5 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyBKjwVhLCwkwmbC6IWiV9upggfVfm96lBM",
+  apiKey: "AIzaSyDxeU1ZG-SXr_H4v-96JqWioj6SX-x1KrM",
   authDomain: "gifted-airport-467106-h6.firebaseapp.com",
   projectId: "gifted-airport-467106-h6",
   storageBucket: "gifted-airport-467106-h6.firebasestorage.app",

1e7c163a1a775f888a1ab5fed01fba78f7ead271
Your Name <aidarabdrahim1@gmail.com>
2026-03-09 05:10:59 +0000

    AIzaSyBKjwVhLCwkwmbC6IWiV9upggfVfm96lBM api key gifted-airport-467106-h6

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBKjwVhLCwkwmbC6IWiV9upggfVfm96lBM",
-  authDomain: "studio-5303827652-38d67.firebaseapp.com",
-  projectId: "studio-5303827652-38d67",
-  storageBucket: "studio-5303827652-38d67.firebasestorage.app",
+  authDomain: "gifted-airport-467106-h6.firebaseapp.com",
+  projectId: "gifted-airport-467106-h6",
+  storageBucket: "gifted-airport-467106-h6.firebasestorage.app",
   messagingSenderId: "434230863781",
   appId: "1:434230863781:web:17ed6d12afbccb348f2a4c",
   measurementId: ""

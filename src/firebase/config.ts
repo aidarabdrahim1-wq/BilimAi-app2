19b7f6b2468ec4a70f8b38aab196249fc8ba78b1
Your Name <aidarabdrahim1@gmail.com>
2026-03-09 05:16:10 +0000

    const firebaseConfig = {   apiKey: "AIzaSyD4h_rErcBSKWhiI1U3CBpF8vDpWAfM

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDxeU1ZG-SXr_H4v-96JqWioj6SX-x1KrM",
-  authDomain: "gifted-airport-467106-h6.firebaseapp.com",
-  projectId: "gifted-airport-467106-h6",
-  storageBucket: "gifted-airport-467106-h6.firebasestorage.app",
+  apiKey: "AIzaSyD4h_rErcBSKWhiI1U3CBpF8vDpWAfMRMY",
+  authDomain: "studio-5303827652-38d67.firebaseapp.com",
+  databaseURL: "https://studio-5303827652-38d67-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "studio-5303827652-38d67",
+  storageBucket: "studio-5303827652-38d67.firebasestorage.app",
   messagingSenderId: "434230863781",
   appId: "1:434230863781:web:17ed6d12afbccb348f2a4c",
   measurementId: ""

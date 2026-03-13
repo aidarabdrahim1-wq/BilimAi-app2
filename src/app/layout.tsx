1bb5d1a807cbba06b6b39e9aa6c02e295cbbb6ce
41ParadoxAidar abdrahim79Весторес <aidarabdrahim1@gmail.com>
2026-03-13 16:50:47 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           strategy="beforeInteractive" 
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <FirebaseClientProvider>
           <AuthProvider>
             <FirebaseErrorListener />

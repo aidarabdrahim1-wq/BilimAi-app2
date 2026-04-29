14f94a7a0a4b9e504e4c744a981af23d376816a5
41ParadoxAidar abdrahim79Весторес <aidarabdrahim1@gmail.com>
2026-04-29 13:33:20 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD4h_rErcBSKWhiI1U3CBpF8vDpWAfMRMY",
-  authDomain: "studio-5303827652-38d67.firebaseapp.com",
-  databaseURL: "https://studio-5303827652-38d67-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "studio-5303827652-38d67",
-  storageBucket: "studio-5303827652-38d67.firebasestorage.app",
-  messagingSenderId: "434230863781",
-  appId: "1:434230863781:web:17ed6d12afbccb348f2a4c",
-  measurementId: ""
+  "projectId": "studio-5303827652-38d67",
+  "appId": "1:434230863781:web:17ed6d12afbccb348f2a4c",
+  "apiKey": "AIzaSyD4h_rErcBSKWhiI1U3CBpF8vDpWAfMRMY",
+  "authDomain": "studio-5303827652-38d67.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "434230863781"
 };
